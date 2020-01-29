cecf284eb4fdfc0323b0be5e4e8d55b30833ca39
Han Mai <cafe2310@gmail.com>
2020-01-29 18:22:43 +0700

    First implementation

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # hashable-queue
-A simple class that supports fast access and simple function to remove the first in queue
+A simple class that supports fast access and simple function to remove the first element in queue.
+
+`hashable-queue` is a hybrid class of `queue` and `unordered_map`. It provides most of necessary functions to maintain a queue that is capable of random access by key.
+
+```C++
+bool empty() const;
+size_type size() const;
+value_type &front();
+value_type &back();
+void pop();
+value_type &operator[](const key_type &key);
+```
+
+*The source code has not been tested yet*
---
+++ hashable-queue.hpp
@@ -0,0 +1,59 @@
+#ifndef HASHABLE_QUEUE_HPP
+#define HASHABLE_QUEUE_HPP
+
+#include <queue>
+#include <list>
+#include <unordered_map>
+
+template <class K, class V>
+class hashable_queue
+{
+public:
+    bool empty() const
+    {
+        return this->m_keys.empty();
+    }
+
+    size_type size() const
+    {
+        return this->m_keys.size();
+    }
+
+    V &front()
+    {
+        auto &k = this->m_keys.front();
+        return this->m_map[k];
+    }
+
+    V &back()
+    {
+        auto &k = this->m_keys.back();
+        return this->m_map[k];
+    }
+
+    void pop()
+    {
+        if (!this->empty())
+        {
+            auto &k = this->m_keys.front();
+            this->m_map.erase(k);
+            this->m_keys.pop();
+        }
+    }
+
+    V &operator[](const K &k)
+    {
+        auto &v = this->m_map[k];
+        if (this->m_map.size() > this->m_keys.size())
+        {
+            this->m_keys.push(k);
+        }
+        return v;
+    }
+
+protected:
+    std::queue<K, std::list> m_keys;
+    std::unordered_map<K, V> m_map;
+};
+
+#endif // HASHABLE_QUEUE_HPP
